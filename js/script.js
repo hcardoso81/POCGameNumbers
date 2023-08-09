16d2175c77735e2c61665409997fc4f3e63ebb62
MS\hcardoso <hernan.f.cardoso@gmail.com>
2023-08-09 18:36:30 -0300

    add behavior to buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 "use strict"
 
+var numberUser = [];
 const generateRandomNumbers = () => {
     const randomNumbers = [];
 
@@ -10,11 +11,53 @@ const generateRandomNumbers = () => {
             randomNumbers.push(randomNumber);
         }
     }
+    randomNumbers.forEach((number, index) => {
+        document.getElementById('numberSecret' + index).innerHTML = number;
 
-    return randomNumbers;
+    });
+
+
 };
 
+const clickCleanButton = () => {
+    const cleanButton = document.getElementById('cleanButton');
+    cleanButton.addEventListener("click", (event) => {
+        alert('Limpiar');
+    });
+};
+
+const clickInvestigateButton = () => {
+    const cleanButton = document.getElementById('investigateButton');
+    cleanButton.addEventListener("click", (event) => {
+        alert('Indagar');
+    });
+};
+
+const clickConfirmButton = () => {
+    const cleanButton = document.getElementById('confirmButton');
+    cleanButton.addEventListener("click", (event) => {
+        alert('confirmar');
+    });
+};
+
+const clickNumbersButton = () => {
+    const numbersButton = document.querySelectorAll("button.numberSelected");
+    numbersButton.forEach((numberButton) =>
+        numberButton.addEventListener("click", (event) => {
+            alert(numberButton.textContent);
+        }));
+};
+
+const addEventListenerToButtons = () => {
+    clickCleanButton();
+    clickInvestigateButton();
+    clickConfirmButton();
+    clickNumbersButton();
+}
+
 window.addEventListener("DOMContentLoaded", () => {
-    const randomArray = generateRandomNumbers();
-    console.log(randomArray);
+    generateRandomNumbers();
+    addEventListenerToButtons();
+
+
 });
